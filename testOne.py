25fcb585e9464292cc3a22ac73339f16e531d5db
MarCodesM <74581876+MarCodesM@users.noreply.github.com>
2021-11-23 17:03:52 -0600

    Add files via upload

---
+++ testOne.py
@@ -0,0 +1,18 @@
+def printWelcome():
+    print("\tPanda Protection")
+    print("1) Create Profile")
+    print("2) Login")
+    print("3) Exit")
+def printCreateProfile():
+    print("\n\tCreate Profile")
+
+printWelcome()
+userinput = input("Enter Choice: ")
+
+printCreateProfile()
+un = input("Enter Username: ")
+pw = input("Enter Password: ")
+
+
+
+
